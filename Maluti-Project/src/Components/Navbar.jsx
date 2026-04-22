fec338ec42dc6aa0298303efa5182cd2e6cb7f8e
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-04-22 08:26:20 +0200

    Added a logo on the navbar

---
+++ Maluti-Project/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { FiSearch, FiChevronDown } from "react-icons/fi";
+import "../styles/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      
+      <div className="logo">
+        <img src="/images/logo.png" alt="Logo" />
+      </div>
+
