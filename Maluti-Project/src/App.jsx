6a63ad9607d37c49e4450b144b8db1a8f9be85b8
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-04-22 12:07:33 +0200

    Added QuickAccess component import and usage in App.jsx

--- Maluti-Project/src/App.jsx
+++ Maluti-Project/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import QuickAccess from "./Components/QuickAccess";
 import "./App.css";  
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <QuickAccess />
     </>
   );
 }
