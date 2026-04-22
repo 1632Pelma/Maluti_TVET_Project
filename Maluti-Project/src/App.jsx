0b7245a4825d99f4f9074af69460294981c5d3e1
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-04-22 12:54:25 +0200

    Added Announcements component import and usage in App.jsx

--- Maluti-Project/src/App.jsx
+++ Maluti-Project/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
 import QuickAccess from "./Components/QuickAccess";
+import Announcements from "./Components/Announcements";
 import "./App.css";  
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <QuickAccess />
+      <Announcements />
     </>
   );
 }
