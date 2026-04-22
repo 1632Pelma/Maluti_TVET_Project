d94db4f9a691f7ff3abdae2b8a18a4b30edbd356
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-04-22 08:27:33 +0200

    Added navigation links for Home, Apply, Courses and more

--- Maluti-Project/src/Components/Navbar.jsx
+++ Maluti-Project/src/Components/Navbar.jsx
@@ -9,3 +9,14 @@ export default function Navbar() {
         <img src="/images/logo.png" alt="Logo" />
       </div>
 
+      
+      <div className="nav-right">
+        <div className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Study</a>
+          <a href="#">Apply</a>
+          <a href="#">Campuses</a>
+          <a href="#">Student Life</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
