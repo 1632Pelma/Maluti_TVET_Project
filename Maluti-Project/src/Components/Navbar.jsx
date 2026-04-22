ab7c896ad20fe76900c3a487c8068ba9a75342e2
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-04-22 08:28:16 +0200

    Added  a dropdown menu for Staff

--- Maluti-Project/src/Components/Navbar.jsx
+++ Maluti-Project/src/Components/Navbar.jsx
@@ -17,6 +17,22 @@ export default function Navbar() {
           <a href="#">Apply</a>
           <a href="#">Campuses</a>
           <a href="#">Student Life</a>
+
+        <div className="dropdown">
+  <span className="dropdown-toggle">
+    Staff <FiChevronDown className="dropdown-icon" />
+  </span>
+
+  <div className="dropdown-menu">
+    <a href="#">Vacancies</a>
+    <a href="#">Tenders</a>
+    <a href="#">Publications</a>
+    <a href="#">Staff QMS</a>
+    <a href="#">Fraud Hotline</a>
+    <a href="#">IT Satisfaction Survey</a>
+  </div>
+</div>
+
           <a href="#">About</a>
           <a href="#">Contact</a>
         </div>
