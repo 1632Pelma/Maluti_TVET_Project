ca69859934ff3f60580ab3ea38ee8c2259086ab0
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-04-22 17:57:43 +0200

    Added routes for Programmes, Events, Stats, Footer links

--- Maluti-Project/src/App.jsx
+++ Maluti-Project/src/App.jsx
@@ -3,6 +3,10 @@ import Hero from "./Components/Hero";
 import About from "./Components/About";
 import QuickAccess from "./Components/QuickAccess";
 import Announcements from "./Components/Announcements";
+import Programmes from "./Components/Programmes";
+import Events from "./Components/Events";
+import Stats from "./Components/Stats";
+import FooterLinks from "./Components/FooterLinks";
 import "./App.css";  
 
 function App() {
@@ -12,7 +16,11 @@ function App() {
       <Hero />
       <About />
       <QuickAccess />
+      <Programmes />
       <Announcements />
+      <Events />
+      <Stats />
+      <FooterLinks />
     </>
   );
 }
